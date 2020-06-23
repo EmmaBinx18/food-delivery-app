588919fbbf55e0f096865560467eb849fea92233
Emma Coetzer <emmac@bbd.co.za>
2020-06-23 12:48:58 +0200

    chore: removed carousel component

--- front-end/feed-me/src/app/modules/home/home.module.ts
+++ front-end/feed-me/src/app/modules/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { CarouselComponent } from './carousel/carousel.component';
 import { MealCategoriesComponent } from './meal-categories/meal-categories.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { OurServicesComponent } from './our-services/our-services.component';
@@ -10,7 +9,7 @@ import { OurServicesComponent } from './our-services/our-services.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-    declarations: [HomeComponent, CarouselComponent, MealCategoriesComponent, AboutUsComponent, OurServicesComponent],
+    declarations: [HomeComponent, MealCategoriesComponent, AboutUsComponent, OurServicesComponent],
     imports: [
         SharedModule,
         HomeRoutingModule
