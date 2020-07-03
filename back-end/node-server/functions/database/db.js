6d23e257b9c3f9eca1dfb53aec059ef7943dc34e
Emma Coetzer <emmac@bbd.co.za>
2020-07-03 20:56:37 +0200

    added database files

---
+++ back-end/node-server/functions/database/db.js
@@ -0,0 +1,33 @@
+const sql = require("mssql");
+const config = require("./db-config");
+const logger = require('../logger/winstin.logger');
+
+module.exports = {
+    executeStoredProcedure(storedProcedure, parameters, callback){
+        sql.connect(config, (error) => {
+            if(error) {
+                sql.close();
+                logger.error("DATABASE ERROR", error);
+                throw new Error(error);
+            }
+            
+            const request = new sql.Request();
+            request.input("JSON", sql.VarChar(sql.MAX), JSON.stringify(parameters));
+            request.output("Error", sql.Int);
+            request.execute(storedProcedure, (err, recordSets, returnValue) => {
+                if(err){
+                    sql.close();
+                    logger.error("DATABASE ERROR", err);
+                    throw new Error(err);
+                }
+                else{
+                    sql.close();
+                    return callback(this.mapReturnData(recordSets.recordset));
+                }
+            });
+        });
+    },
+    mapReturnData(data){
+        return data[0]['JSON_F52E2B61-18A1-11d1-B105-00805F49916B'];
+    }
+}
